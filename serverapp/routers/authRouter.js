4baac67ea5e23d4563b19585bf223f25a175ff7f
bitotassuya <jareansong@gmail.com>
2022-07-04 21:34:37 +0700

    ExistingUser P1

--- serverapp/routers/authRouter.js
+++ serverapp/routers/authRouter.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const validateForm = require("../controllers/validateForm");
 const router = express.Router();
+const pool = require("../database")
+const bcrypt = require("bcrypt")
+const { RESERVED_EVENTS } = require("socket.io/dist/socket");
 
 router.post("/login", (req, res) => {
     validateForm(req, res);
@@ -8,5 +11,22 @@ router.post("/login", (req, res) => {
 
 router.post("/signup", (req, res) => {
     validateForm(req, res);
+
+
+    const existingUser = await pool.query(
+        "SELECT username from users WHERE username=$1",
+        [req.body.username]
+    );
+    if (existingUser.rowCount === 0) {
+        //register
+        const hashedPass = await bcrypt.hash(req.body.password, 10);
+        const newUserQuery = await pool.query(
+            "INSERT INTO users(username,passhash) values($1,$2) RETURNING username",
+            [req.body.username, hashedPass]
+        );
+    }
+    else {
+        res.json({ loggedIn: false, status: "Username taken" });
+    }
 });
 module.exports = router;
